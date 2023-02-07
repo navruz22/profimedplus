f33a27fd18c3c20c96c48183c1bf5f92ab238fae
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2023-02-07 22:42:06 +0500

    csc

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ const { start } = require('./connectDB/db')
 start(app)
 
 const { routers } = require('./routers/routers')
+const path = require('path')
 routers(app)
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "./frontend", "build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "./frontend", "build", "index.html")
+        );
+    });
+}
