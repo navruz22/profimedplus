5adab34a09565022aad0d9152ffe5c85f0672370
navruz22 <navruz.ochilov98@mail.ru>
2025-03-26 18:34:05 +0500

    done

--- frontend/src/clinica/sections/director/adver/StatusClients.js
+++ frontend/src/clinica/sections/director/adver/StatusClients.js
@@ -82,7 +82,7 @@ const StatusClients = () => {
     const [currentClients, setCurrentClients] = useState([])
     const [searchStorage, setSearchStrorage] = useState([])
 
-    const getConnectors = useCallback(async (beginDay, endDay) => {
+    const getConnectors = async (beginDay, endDay) => {
         try {
             const data = await request(
                 `/api/adver/status/clients`,
@@ -101,7 +101,7 @@ const StatusClients = () => {
                 status: 'error',
             })
         }
-    }, [request, auth, notify, setSearchStrorage])
+    }
 
     //=================================================
     //=================================================
@@ -194,20 +194,13 @@ const StatusClients = () => {
 
     const changeStart = (e) => {
         setBeginDay(new Date(new Date(e).setUTCHours(0, 0, 0, 0)));
-        getConnectors(new Date(new Date(e).setUTCHours(0, 0, 0, 0)), endDay);
+        // getConnectors(new Date(new Date(e).setUTCHours(0, 0, 0, 0)), endDay);
     };
 
     const changeEnd = (e) => {
-        const date = new Date(
-            new Date(new Date().setDate(new Date(e).getDate() + 1)).setUTCHours(
-                0,
-                0,
-                0,
-                0
-            )
-        );
+        const date = new Date(new Date(e).setUTCHours(23, 59, 59, 59))
         setEndDay(date);
-        getConnectors(beginDay, date);
+        // getConnectors(beginDay, date);
     };
 
     const [s, setS] = useState()
@@ -264,34 +257,45 @@ const StatusClients = () => {
         setMaxAge(max_age)
     }
 
-    const setSearch = () => {
+    const searchAll = (data) => {
         const getAge = age => new Date().getFullYear() - new Date(age?.client?.born).getFullYear()
         if (gender && !national) {
-            setCurrentClients([...searchStorage].filter(connector =>
+            setCurrentClients([...data].filter(connector =>
                 getAge(connector) >= minAge && getAge(connector) <= maxAge && connector?.client?.gender === gender
             ))
         } else if (national && !gender) {
-            setCurrentClients([...searchStorage].filter(connector =>
+            setCurrentClients([...data].filter(connector =>
                 getAge(connector) >= minAge && getAge(connector) <= maxAge && (connector?.client?.national && connector?.client?.national === national)
             ))
-            // } else if (gender && national && !age) {
-            //     setCurrentClients([...searchStorage].filter(connector =>
-            //         connector?.client?.gender === gender && (connector?.client?.national && connector?.client?.national === national)
-            //     ))
-            // } else if (age && gender && national) {
-            //     setCurrentClients([...searchStorage].filter(connector =>
-            //         new Date().getFullYear() - new Date(connector?.client?.born).getFullYear() === age && (connector?.client?.national && connector?.client?.national === national) && connector?.client?.gender === gender
-            //     ))
+        } else if (gender && national) {
+            setCurrentClients([...data].filter(connector =>
+                getAge(connector) >= minAge && getAge(connector) <= maxAge && connector?.client?.gender === gender && (connector?.client?.national && connector?.client?.national === national)
+            ))
         } else {
-            // age && setCurrentClients([...searchStorage].filter(connector =>
-            //     new Date().getFullYear() - new Date(connector?.client?.born).getFullYear() === age
-            // ))
-            national && setCurrentClients([...searchStorage].filter(connector =>
-                connector?.client?.national && connector?.client?.national === national
+            setCurrentClients([...data].filter(connector =>
+                getAge(connector) >= minAge && getAge(connector) <= maxAge
             ))
-            gender && setCurrentClients([...searchStorage].filter(connector =>
-                connector?.client?.gender === gender
-            ))
+        }
+    }
+
+    const setSearch = async () => {
+        try {
+            const data = await request(
+                `/api/offlineclient/client/getall`,
+                'POST',
+                { clinica: auth.clinica._id, beginDay, endDay, status: state.status },
+                {
+                    Authorization: `Bearer ${auth.token}`,
+                },
+            )
+            setSearchStrorage(data)
+            searchAll(data)
+        } catch (error) {
+            notify({
+                title: t(`${error}`),
+                description: '',
+                status: 'error',
+            })
         }
     }
 
--- frontend/src/clinica/sections/director/clients/OfflineClients.js
+++ frontend/src/clinica/sections/director/clients/OfflineClients.js
@@ -196,18 +196,11 @@ const OfflineClients = () => {
     };
 
     const changeEnd = (e) => {
-        const date = new Date(
-            new Date(new Date().setDate(new Date(e).getDate() + 1)).setUTCHours(
-                0,
-                0,
-                0,
-                0
-            )
-        );
+        const date = new Date(new Date(e).setUTCHours(23, 59, 59, 59))
         setEndDay(date);
         // getConnectors(beginDay, date);
     };
-
+    console.log(endDay);
     const [s, setS] = useState()
     useEffect(() => {
         if (!s) {
